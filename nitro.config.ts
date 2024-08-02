2b792be3fdb6c428dc717dfef104b4d13e9a1f48
klarstil <klarstil@googlemail.com>
2024-08-02 10:20:00 +0200

    feat: redirect request instead of proxying it

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,12 +1,4 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
   srcDir: "server",
-  routeRules: {
-    '/store-api/**': {
-      cache: {
-        maxAge: 60 * 1 * 60,
-        swr: true
-      }
-    }
-  }
 });
--- server/routes/store-api/[...].ts
+++ server/routes/store-api/[...].ts
@@ -1,8 +1,38 @@
-import { proxyRequest } from "#imports";
+import { defineCachedEventHandler } from "#imports";
 
-export default defineEventHandler(async (event) => {
-    const target = `https://korodrogerie.de${event.path}`;
-    const response = await proxyRequest(event, target);
+export default defineCachedEventHandler(async (event) => {
+    const target = `https://www.korodrogerie.de${event.path}`;
 
-    return response;
+    const body = await readBody(event);
+    const headers = getRequestHeaders(event);
+
+    const requestHeaders = {};
+
+    if (Object.keys(headers).includes('sw-access-key') && headers['sw-access-key']) {
+        requestHeaders['sw-access-key'] = headers['sw-access-key'];
+    }
+
+    if (Object.keys(headers).includes('sw-language-id') && headers['sw-language-id']) {
+        requestHeaders['sw-language-id'] = headers['sw-language-id'];
+    }
+
+    if (Object.keys(headers).includes('sw-context-token') && headers['sw-context-token']) {
+        requestHeaders['sw-context-token'] = headers['sw-context-token'];
+    }
+
+    try {
+        const response = await $fetch(target, {
+            method: event.method,
+            body: JSON.stringify(body),
+            headers: requestHeaders
+        });
+
+        return response;
+    } catch(err) {
+        console.log(err);
+    }
+}, {
+    maxAge: 60 * 1 * 60,
+    swr: true,
+    varies: ['sw-access-key', 'sw-language-id', 'sw-context-token']
 });
